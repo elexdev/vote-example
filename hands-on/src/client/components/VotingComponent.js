ba0abf1223ad6bc9b777fcce786b4a054f948f99
Alexander Kaiser <elexd3v@gmail.com>
2017-02-13 21:41:17 +0100

    more static component stuff

---
+++ hands-on/src/client/components/VotingComponent.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import ChoiceBar from './ChoiceBar';
+
+export default class VotingComponent extends Component {
+	render() {
+		const {vote} = this.props;
+		return (
+			<div className="Row VotingRow Spacer">
+				<div className="Head">
+					<h1 className="Title">{vote.title}
+						<div className="Badge">{vote.totalVotes}</div>
+					</h1>
+					<div className="Description Emphasis">
+						{vote.description}
+					</div>
+				</div>
+				<div>
+					{vote.choices.map(c => <ChoiceBar key={c.id} {...c} />)}
+				</div>
+			</div>
+		);
+	}
+}
+
+VotingComponent.propTypes = {
+	vote: React.PropTypes.object.isRequired
+};
